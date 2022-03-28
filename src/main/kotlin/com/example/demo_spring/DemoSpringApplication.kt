742ad47e5484127f78ec24ff878d3b562c650a94
oracl12 <ostapko2003o@gmail.com>
2022-03-28 22:29:28 +0300

    asd

---
+++ src/main/kotlin/com/example/demo_spring/DemoSpringApplication.kt
@@ -0,0 +1,21 @@
+package com.example.demo_spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class DemoSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoSpringApplication>(*args)
+}
+
+@RestController
+class TestController {
+    @RequestMapping("/")
+    fun bestapi(): String {
+        return "asdasdasd"
+    }
+}
