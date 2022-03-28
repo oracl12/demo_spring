871c855beebad61b113be2b5e552a4e733786864
oracl12 <ostapko2003o@gmail.com>
2022-03-28 22:35:16 +0300

    suka

--- src/main/kotlin/com/example/demo_spring/DemoSpringApplication.kt
+++ src/main/kotlin/com/example/demo_spring/DemoSpringApplication.kt
@@ -11,7 +11,7 @@ class DemoSpringApplication
 fun main(args: Array<String>) {
     runApplication<DemoSpringApplication>(*args)
 }
-
+//adasdasd
 @RestController
 class TestController {
     @RequestMapping("/")
